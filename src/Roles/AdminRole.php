01556ada83ae7ed61bc1e3dd31f1627083b93f2c
Axel Pardemann <axel.pardemann@norse.blue>
2020-11-07 10:13:15 -0600

    Fix styling

--- src/Roles/AdminRole.php
+++ src/Roles/AdminRole.php
@@ -6,7 +6,8 @@ namespace NorseBlue\Heimdall\Roles;
 
 class AdminRole extends DefinedRole
 {
-    public static function key(): string {
+    public static function key(): string
+    {
         return 'admin';
     }
 
