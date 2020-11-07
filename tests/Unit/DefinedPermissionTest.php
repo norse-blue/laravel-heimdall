d98774a5bd5b95741d9160e4ee38232d90e09ddc
Axel Pardemann <axel.pardemann@norse.blue>
2020-11-07 10:22:45 -0600

    Add missing tests

--- tests/Unit/DefinedPermissionTest.php
+++ tests/Unit/DefinedPermissionTest.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 use NorseBlue\Heimdall\Permission;
 use NorseBlue\Heimdall\Permissions\Admin\Dashboard\DashboardShowPermission;
 use NorseBlue\Heimdall\Permissions\Admin\Users\UsersCreatePermission;
+use NorseBlue\Heimdall\Permissions\Admin\Users\UsersDestroyPermission;
+use NorseBlue\Heimdall\Permissions\Admin\Users\UsersEditPermission;
+use NorseBlue\Heimdall\Permissions\Admin\Users\UsersListPermission;
+use NorseBlue\Heimdall\Permissions\Admin\Users\UsersShowPermission;
 use function Pest\Laravel\mock;
 
-it('can create a defined admin view dashboard permission', function () {
+it('can create a defined admin dashboard show permission', function () {
     mock('translator', function ($mock) {
         $mock->shouldReceive('get')->andReturnUsing(fn ($text) => $text);
         $mock->shouldReceive('trans')->andReturnUsing(fn ($text) => $text);
@@ -19,7 +23,7 @@ it('can create a defined admin view dashboard permission', function () {
     $this->assertEquals(DashboardShowPermission::definition(), $permission->jsonSerialize());
 });
 
-it('can create a defined create users permission', function () {
+it('can create a defined admin users create permission', function () {
     mock('translator', function ($mock) {
         $mock->shouldReceive('get')->andReturnUsing(fn ($text) => $text);
         $mock->shouldReceive('trans')->andReturnUsing(fn ($text) => $text);
@@ -28,4 +32,48 @@ it('can create a defined create users permission', function () {
 
     $this->assertInstanceOf(Permission::class, $permission);
     $this->assertEquals(UsersCreatePermission::definition(), $permission->jsonSerialize());
+});
+
+it('can create a defined admin users destroy permission', function () {
+    mock('translator', function ($mock) {
+        $mock->shouldReceive('get')->andReturnUsing(fn ($text) => $text);
+        $mock->shouldReceive('trans')->andReturnUsing(fn ($text) => $text);
+    });
+    $permission = new UsersDestroyPermission();
+
+    $this->assertInstanceOf(Permission::class, $permission);
+    $this->assertEquals(UsersDestroyPermission::definition(), $permission->jsonSerialize());
+});
+
+it('can create a defined admin users edit permission', function () {
+    mock('translator', function ($mock) {
+        $mock->shouldReceive('get')->andReturnUsing(fn ($text) => $text);
+        $mock->shouldReceive('trans')->andReturnUsing(fn ($text) => $text);
+    });
+    $permission = new UsersEditPermission();
+
+    $this->assertInstanceOf(Permission::class, $permission);
+    $this->assertEquals(UsersEditPermission::definition(), $permission->jsonSerialize());
+});
+
+it('can create a defined admin users list permission', function () {
+    mock('translator', function ($mock) {
+        $mock->shouldReceive('get')->andReturnUsing(fn ($text) => $text);
+        $mock->shouldReceive('trans')->andReturnUsing(fn ($text) => $text);
+    });
+    $permission = new UsersListPermission();
+
+    $this->assertInstanceOf(Permission::class, $permission);
+    $this->assertEquals(UsersListPermission::definition(), $permission->jsonSerialize());
+});
+
+it('can create a defined admin users show permission', function () {
+    mock('translator', function ($mock) {
+        $mock->shouldReceive('get')->andReturnUsing(fn ($text) => $text);
+        $mock->shouldReceive('trans')->andReturnUsing(fn ($text) => $text);
+    });
+    $permission = new UsersShowPermission();
+
+    $this->assertInstanceOf(Permission::class, $permission);
+    $this->assertEquals(UsersShowPermission::definition(), $permission->jsonSerialize());
 });
